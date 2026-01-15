e24279522f7eaa4c8192f2c6518078e9c429d5a5
Mehedi Hasan Shawon <mehedi@orangetoolz.com>
2026-01-15 14:13:47 +0600

    feat: change port

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`JSON Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}/contacts`);
 });
